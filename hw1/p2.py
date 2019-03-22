96b8462b25f6de5afa4aa4b64ad2fb0917547065
AlexYang <alex10130010@gmail.com>
2019-03-22 23:08:47 +0800

    advance coding & better performance

--- hw1/p2.py
+++ hw1/p2.py
@@ -3,38 +3,30 @@ import matplotlib.pyplot as plt
 import copy
 
 data = [line.strip('\n').split('\t') for line in open('./data2.txt','r')]
-x1, x2, x3, x4, y = zip(*data)
-x, y = np.array([[1., x1[i], x2[i], x3[i], x4[i]] for i in range(len(x1))]).astype(float), list(map(float, y))
-# x1, x2, x3, x4, y = list(map(float, x1)), list(map(int, x2)), list(map(float, x3)), list(map(float, x4)), list(map(float, y))
+x = np.vstack(data)[:,0:4]
+x = np.concatenate((np.ones((x.shape[0], 1)), x), 1).astype(np.float32)
+y = np.vstack(data)[:,4].astype(np.float32)
 
-iter_num = 20000
-lr = 0.000022089
-init_w = np.array([0.0 for i in range(5)])
-print('Initialize w as [ %s ],\n running %d iteration and the learning rate is %f' 
+iter_num = 15000000
+lr = 0.0000002
+init_w = np.zeros(5).astype(np.float32)
+print('Initialize w as [ %s ],\n running %d iteration and the learning rate is %.7f' 
     % (', '.join(str(w) for w in init_w), iter_num, lr))
-def f(_X): return np.dot(_X, init_w)
 
 for i in range(iter_num):
-    grad_w = np.array([0.0 for i in range(5)])
-    err = 0.0
-    for j in range(len(data)):
-        temp = y[j] - f(x[j])
-        for k in range(len(grad_w)):
-            if k == 0:
-                grad_w[k] -= (2 / len(data)) * temp
-            else:
-                grad_w[k] -= (2 / len(data)) * temp * x[j][k]
-        err += temp ** 2
-    for l in range(len(grad_w)):
-        init_w[l] -= lr * grad_w[l]
-    err /= len(data)
-    if i % 500 == 499:
-        print('[%d/%d] W(w0, w1, w2, w3, w4) = (%s), Err = %.4f'
-            % (i+1, iter_num, ', '.join(str(round(w, 4)) for w in init_w), err))
-print('Result: Weight (w0, w1, w2, w3, w4) = (%s)' % (', '.join(str(round(w, 4)) for w in init_w)))
+    loss = np.dot(x, init_w) - y
+    grad_w = 2.0 * np.dot(x.T, loss)
+    init_w -= lr * grad_w
 
+    if i % 500 == 499:
+        print('[%d/%d] loss = %.4f\r'
+            % (i+1, iter_num, np.mean(np.square(loss))), end='')
+print('Result: Weight (w0, w1, w2, w3, w4) = (%s), loss = %.4f' 
+    % (', '.join(str(round(w, 4)) for w in init_w), np.mean(np.square(loss))))
+
+# cheat_ans = np.matmul(np.matmul(np.linalg.inv(np.matmul(x.T, x)), x.T), y)
 for i in [[6.8, 210, 0.402, 0.739], [6.1, 180, 0.415, 0.713]]:
-    _x = copy.copy(i)
+    _x = copy.deepcopy(i)
     _x.insert(0, 1.)
     print('When (x1, x2, x3, x4) = %s, the predicted value will be %.1f' 
-        % (', '.join(str(round(_i, 4)) for _i in i), f(_x)))
+        % (', '.join(str(round(_i, 4)) for _i in i), np.dot(_x, init_w)))
